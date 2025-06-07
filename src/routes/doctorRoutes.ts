2799d26a2dc94b970c355f3e50713ca3f1b01c43
PedroGabrielM <pedrombalduino@gmail.com>
2025-06-07 17:34:57 -0300

    Adjusted Routes.

--- src/routes/doctorRoutes.ts
+++ src/routes/doctorRoutes.ts
@@ -12,10 +12,40 @@ const ctrl = new DoctorController();
  *   description: Gerenciamento de médicos
  */
 
+/**
+ * @swagger
+ * /api/doctors:
+ *   post:
+ *     summary: Cria um novo médico 
+ *     tags: [Doctors]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CreateDoctorsDto'
+ *     responses:
+ *       201:
+ *         description: Médico criado com sucesso
+ *       409:
+ *         description: Email já cadastrado
+ *       422:
+ *         description: Payload inválido
+ */
 router.post("/", (req, res, next) =>
   ctrl.create(req, res, next)
 );
 
+/**
+ * @swagger
+ * /api/Doctors:
+ *   get:
+ *     summary: Lista todos os pacientes
+ *     tags: [Doctors]
+ *     responses:
+ *       200:
+ *         description: Lista de médicosna base de dados
+ */
 router.get("/", (req, res, next) => ctrl.findAll(req, res, next));
 
 export default router;
