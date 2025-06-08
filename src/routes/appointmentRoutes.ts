2cfd7f1ffe0335845383ce1c5e6ee5a4b35b33cd
PedroGabrielM <pedrombalduino@gmail.com>
2025-06-08 05:59:05 -0300

    Adjusted routes and entities

--- src/routes/appointmentRoutes.ts
+++ src/routes/appointmentRoutes.ts
@@ -18,10 +18,45 @@ router.post("/", (req, res, next) =>
   ctrl.create(req, res, next)
 );
 
+/**
+ * @swagger
+ * /api/doctor/{doctorId}:
+ *   get:
+ *     summary: Retorna um médico por ID
+ *     tags: [Appointments]
+ *     description: Retorna os dados de um médico específico com base no ID fornecido.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema: { type: integer }
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Dados do paciente
+ *       404:
+ *         description: Paciente não encontrado
+ */
 router.get("/doctor/:doctorId", (req, res, next) =>
   ctrl.findByDoctor(req, res, next)
 );
 
+/**
+ * @swagger
+ * /api/patient/{patientId}:
+ *   get:
+ *     summary: Retorna um médico por ID
+ *     tags: [Appointments]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema: { type: integer }
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Dados do paciente
+ *       404:
+ *         description: Paciente não encontrado
+ */
 router.get("/patient/:patientId", (req, res, next) =>
   ctrl.findByPatient(req, res, next)
 );
--- src/routes/doctorRoutes.ts
+++ src/routes/doctorRoutes.ts
@@ -13,13 +13,63 @@ const ctrl = new DoctorController();
  *   name: Doctors
  *   description: Gerenciamento de médicos
  */
-
+/**
+ * @swagger
+ * /api/doctors:
+ *   post:
+ *     summary: Cria um novo médico
+ *     tags: [Doctors]
+ *     description: Cria um novo médico no sistema.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CreateDoctorDto'
+ *     responses:
+ *       201:
+ *         description: Médico criado
+ *       409:
+ *         description: Email já cadastrado
+ *       422:
+ *         description: Payload inválido
+ */
 router.post("/", (req, res, next) =>
   ctrl.create(req, res, next)
 );
 
+/**
+ * @swagger
+ * /api/doctors:
+ *   get:
+ *     summary: Lista todos os medicos
+ *     description: Retorna uma lista de todos os médicos cadastrados no sistema.
+ *     tags: [Doctors]
+ *     responses:
+ *       200:
+ *         description: Lista de pacientes
+ *      
+ */
 router.get("/", (req, res, next) => ctrl.findAll(req, res, next));
 
+/**
+ * @swagger
+ * /api/doctors/{id}:
+ *   get:
+ *     summary: Retorna um médico por ID
+ *     tags: [Doctors]
+ *     description: Retorna os dados de um médico específico com base no ID fornecido.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema: { type: integer }
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Dados do paciente
+ *       404:
+ *         description: Paciente não encontrado
+ */
 router.get("/:id", (req, res, next) => ctrl.findById(req, res, next));
 
 export default router;
